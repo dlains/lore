05cda47df991ac326a00a2de5d3c4ddf2e63b084
David Lains <dlains@gmail.com>
2021-05-26 16:50:24 -0400

    Closes #13 Layout component added. Default document updated.

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,20 @@
 import Link from 'next/link';
+import Head from 'next/head';
+import Layout from '../../components/layout';
+
 
 export default function FirstPost() {
   return (
-    <div className="container mx-auto">
+    <Layout>
+      <Head>
+        <title>First Post - David Lains</title>
+      </Head>
       <h1 className="text-4xl">First Post</h1>
       <h2>
         <Link href="/">
           <a>Back to home.</a>
         </Link>
       </h2>
-    </div>
+    </Layout>
   );
 }
